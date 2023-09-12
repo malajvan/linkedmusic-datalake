1c4d72640c64e54b574fd238c130834324bc9b25
Van Pham <64571718+malajvan@users.noreply.github.com>
2023-09-12 12:03:38 -0400

    Correct jsonld with new simssa dump

--- simssadb/jsonld/PyLD/pyld_test.py
+++ simssadb/jsonld/PyLD/pyld_test.py
@@ -2,68 +2,41 @@ from pyld import jsonld
 import json
 
         
-tested = {
-    "@context":{
-    "@context": {
-        "simssadb": "https://db.simssa.ca/",
-        "mw": "simssadb:musicalworks/",
-        "simssadb_file": "simssadb:files/",
-        "P1476": "wdt:1476",
-        "P86": {
-            "@id": "wdt:P86",
-            "@type": "@id"
-        },
-        "P214": "wdt:P214",
-        "P136": {
-            "@id": "wdt:P136",
-            "@type": "@id"
-        },
-        "database": {"@id":"https://schema.org/Dataset",
-                     "@type":"@id"},
-        "P2701": {
-            "@id": "wdt:P2701",
-            "@type": "@id"
-        },
-        "P2699": "wdt:P2699",
-        "P5426": "wdt:P5426",
-        "wd": "http://www.wikidata.org/entity/",
-        "wdt": "http://www.wikidata.org/prop/direct/",
-        "files": "simssadb_file"
-    }
-},
+tested =     {
+        "@context": "https://raw.githubusercontent.com/malajvan/linkedmusic-datalake/main/simssadb/jsonld/context.jsonld",
         "database": "simssadb:",
         "@type": "wd:Q2188189",
-        "@id": "mw:607",
+        "@id": "mw:4",
         "P86": {
             "@id": "wd:Q2920493",
             "P214": "76504834"
         },
-        "P1476": "['Son io donna (2) qual mostri ogni tuo bene']",
-        "P136": "wd:Q3424719",
+        "P1476": "['Son io donna qual mostri ogni tuo bene']",
+        "P136": "wd:Q4692",
         "files": [
             {
                 "@type": "simssadb_file",
-                "@id": "https://db.simssa.ca/files/1592",
-                "P2701": "wd:Q10610388",
-                "Last_Pitch_Class": "[0.0]"
+                "@id": "https://db.simssa.ca/files/13",
+                "P2701": "wd:Q2115",
+                "Last_Pitch_Class": "[2.0]"
             },
             {
                 "@type": "simssadb_file",
-                "@id": "https://db.simssa.ca/files/1593",
+                "@id": "https://db.simssa.ca/files/14",
+                "P2701": "wd:Q10610388",
+                "Last_Pitch_Class": "[2.0]"
+            },
+            {
+                "@type": "simssadb_file",
+                "@id": "https://db.simssa.ca/files/15",
                 "P2701": "wd:Q42332",
                 "Last_Pitch_Class": None
             },
             {
                 "@type": "simssadb_file",
-                "@id": "https://db.simssa.ca/files/1594",
+                "@id": "https://db.simssa.ca/files/16",
                 "P2701": "wd:Q27967347",
                 "Last_Pitch_Class": None
-            },
-            {
-                "@type": "simssadb_file",
-                "@id": "https://db.simssa.ca/files/1591",
-                "P2701": "wd:Q2115",
-                "Last_Pitch_Class": "[0.0]"
             }
         ]
     }
