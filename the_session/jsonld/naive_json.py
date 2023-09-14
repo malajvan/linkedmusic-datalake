56ef8b48a0d621890ef1ae77af67da8341b8a6fc
Van Pham <64571718+malajvan@users.noreply.github.com>
2023-09-14 12:50:14 -0400

    generate pure json final

--- the_session/jsonld/naive_json.py
+++ the_session/jsonld/naive_json.py
@@ -2,14 +2,21 @@ import pandas as pd
 import json
 
 df = pd.read_csv('../flattening/final_merged.csv')
+float_columns = df.select_dtypes(include=['float']).columns
+
+# Convert float columns to int using pd.Int64Dtype()
+df[float_columns] = df[float_columns].astype(pd.Int64Dtype(), errors='ignore')
+
+
+
 json_data = df.to_json(orient='records')
 parsed_json = json.loads(json_data)
 cleaned_json = [{k: v for k, v in record.items() if v is not None} for record in parsed_json]
 pretty_json = json.dumps(cleaned_json, indent=4)
 
 
-with open('gen_json.json', 'w') as json_file:
-    json_file.write(pretty_json)
+# with open('naive_json.json', 'w') as json_file:
+#     json_file.write(pretty_json)
 
 
 def group_index(names, record):
