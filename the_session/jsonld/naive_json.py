eb110b921a03e55807e72692e7a945f0058771c0
Van Pham <64571718+malajvan@users.noreply.github.com>
2023-09-14 12:26:23 -0400

    generate structured jsonfile

--- the_session/jsonld/naive_json.py
+++ the_session/jsonld/naive_json.py
@@ -11,70 +11,49 @@ pretty_json = json.dumps(cleaned_json, indent=4)
 with open('gen_json.json', 'w') as json_file:
     json_file.write(pretty_json)
 
-grouped_json=[]
-for record in cleaned_json:
-    tunes_data = {}
-    for key, value in record.items():
-        if key.startswith('tunes_'):
+
+def group_index(names, record):
+    rec = {}
+    
+    for name in names:
+        l = []
+
+        for key, value in record.items():
             parts = key.split('_')
+            
+            # Check if the key matches the "name_..._#" pattern
+            if parts[-1].isdigit() and parts[0] == name:
+                index = parts[-1]
+                found = False
 
-            if parts[-1].isdigit() and parts[0] == 'tunes':
-                print(parts)
-                index = f'tunes_settings_{int(parts[-1])}'
-                sub_key = "_".join(parts[0:-1])
-                if index not in tunes_data:
-                    tunes_data[index] = {}
-                tunes_data[index][sub_key] = value
-        else:
-            tunes_data[key] = value
-    grouped_json.append(tunes_data)
+                # Check if the index already exists in the list
+                for s in l:
+                    if list(s)[0].split('_')[-1] == index:
+                        s[key] = value
+                        found = True
+                        break
+                
+                # If the index doesn't exist, create a new dictionary
+                if not found:
+                    l.append({key: value})
+                
+            # If the key isn't an indexed name, add it to the result directly
+            elif not (parts[-1].isdigit() and parts[0] in names):
+                rec[key] = value
 
-# json_keys = [
-#     "file_format_1", "file_format_2", "file_format_3", "file_format_4",
-#     "url_to_file_1", "url_to_file_2", "url_to_file_3", "url_to_file_4",
-#     "Last_Pitch_Class_1", "Last_Pitch_Class_2", "Last_Pitch_Class_3", "Last_Pitch_Class_4"
-# ]
-
-# # Create a nested list of dictionaries
-
-
-# for work in parsed_json:
-#     work["@context"] = "https://raw.githubusercontent.com/malajvan/linkedmusic-datalake/main/simssadb/jsonld/context.jsonld"
-#     work['database'] = 'simssadb:'
-#     work["@type"] = "wd:Q2188189"
-#     work["@id"] = f"mw:{work.pop('musical_work_id')}"
-#     work["P86"] = {
-#         '@id':f'wd:{work.pop("contributor_full_name")}',
-#         'P214': str(int(work["contributor_viaf_id"])) if work["contributor_viaf_id"] else None
-#     }
-#     work.pop("contributor_viaf_id")
-#     work.pop("contributor_auth_url")
-#     work["P1476"] = work.pop("musical_work_variant_titles")
-#     work["P136"] = f'wd:{work.pop("genre_style")}'
+        # Add the list to the result under the name key
+        rec[name] = l
+    
+    return rec
 
 
 
-#     nested_list = []
-#     for i in range(1, 5):
-#         file_format_key = f"file_format_{i}"
-#         url_key = f"url_to_file_{i}"
-#         last_pitch_key = f"Last_Pitch_Class_{i}"
-        
-#         file_format = work.get(file_format_key, None)
-#         url = work.get(url_key, None)
-#         last_pitch = work.get(last_pitch_key, None)
-#         if url is None:
-#             continue
-#         nested_list.append({
-#             "@type": "simssadb_file", 
-#             "@id": url,
-#             'P2701': f'wd:{file_format}',
-#             'Last_Pitch_Class': last_pitch
-#         })
-#     work['files'] = nested_list
-#     for col in json_keys:
-#         del work[col]
-# # Print the nested list of dictionaries
+grouped_json=[]
+names = ['tunes', 'recordings']
+for record in cleaned_json:
+    grouped_json.append(group_index(names, record)) #grouping indexed as a nested json doc/list
+
+    
 pretty_json = json.dumps(grouped_json, indent=4)
 with open('compact.jsonld', 'w') as json_file:
     json_file.write(pretty_json)
