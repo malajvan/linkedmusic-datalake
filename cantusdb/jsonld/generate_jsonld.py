bd47245e4ea4516b7ac5956b09ad82ce0c94e973
Van Pham <64571718+malajvan@users.noreply.github.com>
2023-09-20 09:23:26 -0400

    generate new cantus compact jsonld

--- cantusdb/jsonld/generate_jsonld.py
+++ cantusdb/jsonld/generate_jsonld.py
@@ -2,12 +2,15 @@ import pandas as pd
 import json
 import re
 
-df = pd.read_csv('../link_cantus_reconciled.csv')
+df = pd.read_csv('../reconciled_cantus_09202023.csv')
 json_data = df.to_json(orient='records')
 parsed_json = json.loads(json_data)
 # pretty_json = json.dumps(parsed_json, indent=4)
 # with open('gen_json.json', 'w') as json_file:
 #     json_file.write(pretty_json)
+
+
+
 
 def handle_rec_col(work, val, key):
     if val is None:
@@ -21,40 +24,43 @@ def handle_rec_col(work, val, key):
         return val
 
 
+def create_json_compact(js):
+    for work in js:
 
-for work in parsed_json:
+        work["@context"] = ["https://raw.githubusercontent.com/malajvan/linkedmusic-datalake/main/cantusdb/jsonld/context.jsonld"]
+        work["@id"] = f"chant:{work.pop('chant_id')}" #the @id of each document should be the link to the chant in its database
+        work["@type"] = "wd:Q23072435" #chant
 
-    work["@context"] = ["https://raw.githubusercontent.com/malajvan/linkedmusic-datalake/main/cantusdb/jsonld/context.jsonld"]
-    work["@id"] = f"chant:{work.pop('chant_id')}" #the @id of each document should be the link to the chant in its database
-    work["@type"] = "wd:Q23072435" #chant
-   
-    work['database'] = 'cantusdb:'
-    work["P86"] = "wd:Q4233718" #composer anonymous
-    work["P1922"] = work.pop("incipit")
+        work['database'] = 'cantusdb:'
+        work["P86"] = "wd:Q4233718" #composer anonymous
+        work["P1922"] = work.pop("incipit")
 
-    # work["P136"] = f'wd:{work.pop("genre")}'
-    work["P136"] = handle_rec_col(work,work.pop("genre"), "P136")
-
-
-    # work["Q731978"] = f'wd:{work.pop("mode_name")}'
-    work["Q731978"] = handle_rec_col(work, work.pop("mode_name"), "Q731978")
-
-    work["Q4484726"] = work.pop("finalis") #wikidata Final is closest term to finalis
-
-    work["source"] = f'https://{work.pop("src_link")}'
-    del work["mode"]
-    del work["absolute_url"]
-    del work["composer"]
-    
-    for key in list(work.keys()): #clean up nulls
-        if work[key] is None:
-            del work[key]
-# Print the nested list of dictionaries
-pretty_json = json.dumps(parsed_json, indent=4)
-with open('link_compact.jsonld', 'w') as json_file:
-    json_file.write(pretty_json)
+        # work["P136"] = f'wd:{work.pop("genre")}'
+        work["P136"] = handle_rec_col(work,work.pop("genre"), "P136")
 
 
+        # work["Q731978"] = f'wd:{work.pop("mode_name")}'
+        work["Q731978"] = handle_rec_col(work, work.pop("mode_name"), "Q731978")
+
+        work["Q4484726"] = work.pop("finalis") #wikidata Final is closest term to finalis
+
+        work["source"] = work.pop("src_link")
+
+        work["cantus_id"] = f"https://cantusdatabase.org/id/{work.pop('cantus_id')}"
+        del work["mode"]
+        del work["absolute_url"]
+        del work["composer"]
+        
+        for key in list(work.keys()): #clean up nulls
+            if work[key] is None:
+                del work[key]
+    # Print the nested list of dictionaries
+    return json.dumps(js, indent=4)
+
+
+
+with open('compact.jsonld', 'w') as json_file:
+    json_file.write(create_json_compact(parsed_json))
 
 
 
