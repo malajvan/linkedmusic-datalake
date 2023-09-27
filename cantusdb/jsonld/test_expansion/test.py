a1dfc6a968c36f78cf60ab47d24f4f4ffc7748ee
Van Pham <64571718+malajvan@users.noreply.github.com>
2023-09-27 10:16:50 -0400

    regenerate with new context

--- cantusdb/jsonld/test_expansion/test.py
+++ cantusdb/jsonld/test_expansion/test.py
@@ -3,38 +3,56 @@ import json
 
         
 link_doc =     [
-   {
+    {
+        "incipit": "Fidelia omnia mandata ejus confirmata ",
+        "finalis": "E",
+        "composer": "wd:Q4233718",
+        "absolute_url": "https://cantusdatabase.org/chant/561262",
         "@context": [
-            "https://raw.githubusercontent.com/malajvan/linkedmusic-datalake/main/cantusdb/jsonld/context.jsonld"
+            "https://raw.githubusercontent.com/malajvan/linkedmusic-datalake/new_context/cantusdb/jsonld/context.jsonld"
         ],
-        "@id": "chant:670569",
+        "@id": "chant:561262",
         "@type": "wd:Q23072435",
         "database": "cantusdb:",
-        "P86": "wd:Q4233718",
-        "P1922": "Colligite primum zizania et alligate ",
-        "P136": "wd:Q582093",
-        "Q731978": "wd:Q960729",
-        "Q4484726": "D",
-        "source": "https://cantusdatabase.org/source/669163",
-        "cantus_id": "https://cantusindex.org/id/001853"
+        "genre": "wd:Q582093",
+        "mode_name": "wd:Q1641387",
+        "source": "https://cantusdatabase.org/source/123756",
+        "cantus_id": "https://cantusindex.org/id/002865"
     },
     {
+        "incipit": "Mulieres sedentes ad monumentum lamentabantur ",
+        "finalis": "D",
+        "composer": "wd:Q4233718",
+        "absolute_url": "https://cantusdatabase.org/chant/562284",
         "@context": [
-            "https://raw.githubusercontent.com/malajvan/linkedmusic-datalake/main/cantusdb/jsonld/context.jsonld",
-            {
-                "P136": "wdt:P136"
-            }
+            "https://raw.githubusercontent.com/malajvan/linkedmusic-datalake/new_context/cantusdb/jsonld/context.jsonld"
         ],
-        "@id": "chant:670191",
+        "@id": "chant:562284",
         "@type": "wd:Q23072435",
         "database": "cantusdb:",
-        "P86": "wd:Q4233718",
-        "P1922": "Sicut imber super gramen  et ",
-        "P136": "Tract verse",
-        "Q731978": "wd:Q57976142",
-        "Q4484726": "G",
+        "genre": "wd:Q582093",
+        "mode_name": "wd:Q960729",
+        "source": "https://cantusdatabase.org/source/123756",
+        "cantus_id": "https://cantusindex.org/id/003826"
+    },
+    {
+        "incipit": "Verumtamen justi confitebuntur nomini tuo ",
+        "finalis": "D",
+        "composer": "wd:Q4233718",
+        "absolute_url": "https://cantusdatabase.org/chant/670160",
+        "@context": [
+            "https://raw.githubusercontent.com/malajvan/linkedmusic-datalake/new_context/cantusdb/jsonld/context.jsonld",
+            {
+                "genre": "wdt:P136"
+            }
+        ],
+        "@id": "chant:670160",
+        "@type": "wd:Q23072435",
+        "database": "cantusdb:",
+        "genre": "Tract verse",
+        "mode_name": "wd:Q842766",
         "source": "https://cantusdatabase.org/source/669907",
-        "cantus_id": "https://cantusindex.org/id/g02373b.1"
+        "cantus_id": "https://cantusindex.org/id/g02431j"
     }
 ]
 with open('./expanded.json','w') as injson:
