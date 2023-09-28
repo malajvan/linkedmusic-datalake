41020c8481db993b277e6d3a9b49f4d09dfdd23a
malajvan <vanhongpham01@gmail.com>
2023-09-28 12:04:43 -0400

    fix path

--- simssadb/flattening/SQL_query.py
+++ simssadb/flattening/SQL_query.py
@@ -109,7 +109,7 @@ results = cur.fetchall()
 
 
 # Export data to CSV
-with open('flattened.csv', 'w') as f:
+with open('final_flattened.csv', 'w') as f:
     writer = csv.writer(f)
     writer.writerow([col[0] for col in cur.description])
     writer.writerows(results)
