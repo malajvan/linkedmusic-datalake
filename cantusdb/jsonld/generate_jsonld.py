ae9901d1a107b37214a7ff774b2c588946a843e1
malajvan <vanhongpham01@gmail.com>
2023-09-28 12:02:01 -0400

    add original content to cantus compact

--- cantusdb/jsonld/generate_jsonld.py
+++ cantusdb/jsonld/generate_jsonld.py
@@ -8,44 +8,43 @@ import re
 #       - make changes to the file 'context.jsonld' and make sure to push the 
 #         changes on the appropriate branch (make sure the link below match where you host the context)
 
-df = pd.read_csv('../reconciled_cantus_09202023.csv')
+df = pd.read_csv('../reconciled_cantus_09282023.csv')
 json_data = df.to_json(orient='records')
 parsed_json = json.loads(json_data)
-# pretty_json = json.dumps(parsed_json, indent=4)
-# with open('gen_json.json', 'w') as json_file:
-#     json_file.write(pretty_json)
 
 
-
-
-def handle_rec_col(work, val, key, wID):
+def handle_rec_col(work,key):
+    val = work.pop(key)
+    wID = work.pop(f'{key}_@id')
     if val is None:
         return None
-    if re.match(r"^[Q]\d+", val ): #if cell was reconciled with wikidata
-        return f"wd:{val}"
+    if re.match(r"^[Q]\d+", wID ): #if cell was reconciled with wikidata
+        # work["@context"].append({key:f"wdt:{name_wID}"}) # overwrite context
+        return {"@id":f"wd:{wID}",
+                "name":val}
+        
     if val[:8]=="https://" : #if cell was reconciled with another source
-        return val
+        return {"@id": val}
     else: #cell is value
-        work["@context"].append({key:f"wdt:{wID}"}) # overwrite context
+        # work["@context"].append({key:f"wdt:{wID}"}) # overwrite context
         return val
 
 
 def create_json_compact(js):
     for work in js:
 
-        work["@context"] = ["https://raw.githubusercontent.com/malajvan/linkedmusic-datalake/new_context/cantusdb/jsonld/context.jsonld"]
+        work["@context"] = "https://raw.githubusercontent.com/malajvan/linkedmusic-datalake/add_original_content/cantusdb/jsonld/context.jsonld"
         work["@id"] = f"chant:{work.pop('chant_id')}" #the @id of each document should be the link to the chant in its database
         work["@type"] = "wd:Q23072435" #chant
 
         work['database'] = 'cantusdb:'
-        work["composer"] = "wd:Q4233718" #composer anonymous
+        work["composer"] = handle_rec_col(work, "composer")    #composer anonymous
         # work["P1922"] = work.pop("incipit")
 
-        work["genre"] = handle_rec_col(work,work.pop("genre"), "genre", "P136")
-
+        work["genre"] = handle_rec_col(work,"genre")
 
         # work["Q731978"] = f'wd:{work.pop("mode_name")}'
-        work["mode_name"] = handle_rec_col(work, work.pop("mode_name"), "mode_name", "Q731978")
+        work["mode_name"] = handle_rec_col(work, "mode_name")
 
         # work["Q4484726"] = work.pop("finalis") #wikidata Final is closest term to finalis
 
