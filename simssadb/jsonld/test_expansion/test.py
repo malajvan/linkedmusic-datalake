d040384b5390d76d459e3ef8db22a0e029446e24
malajvan <vanhongpham01@gmail.com>
2023-09-28 13:18:58 -0400

    Update new compact

--- simssadb/jsonld/test_expansion/test.py
+++ simssadb/jsonld/test_expansion/test.py
@@ -2,79 +2,60 @@ from pyld import jsonld
 import json
 
         
-tested =   [    {
-        "musical_work_variant_titles": "['\u039f passi sparsi \u03bf pensier vaghi et pronti']",
-        "@context": [
-            "https://raw.githubusercontent.com/malajvan/linkedmusic-datalake/new_context/simssadb/jsonld/context.jsonld"
-        ],
-        "database": "simssadb:",
-        "@type": "wd:Q2188189",
-        "@id": "mw:25",
-        "composer": "wd:Q7442579",
-        "genre_style": "wd:Q4692",
-        "files": [
-            {
-                "@type": "simssadb_file",
-                "@id": "https://db.simssa.ca/files/100",
-                "file_format": "wd:Q27967347",
-                "Last_Pitch_Class": None
-            },
-            {
-                "@type": "simssadb_file",
-                "@id": "https://db.simssa.ca/files/97",
-                "file_format": "wd:Q2115",
-                "Last_Pitch_Class": "[9.0]"
-            },
-            {
-                "@type": "simssadb_file",
-                "@id": "https://db.simssa.ca/files/98",
-                "file_format": "wd:Q10610388",
-                "Last_Pitch_Class": "[9.0]"
-            },
-            {
-                "@type": "simssadb_file",
-                "@id": "https://db.simssa.ca/files/99",
-                "file_format": "wd:Q42332",
-                "Last_Pitch_Class": None
-            }
-        ]
-    },
+tested =   [    
     {
-        "musical_work_variant_titles": "['Amor quando fioriva mia speme']",
-        "@context": [
-            "https://raw.githubusercontent.com/malajvan/linkedmusic-datalake/new_context/simssadb/jsonld/context.jsonld"
-        ],
+        "musical_work_variant_titles": "['Ave Maria gratia plena \u2026 virgo serena']",
+        "@context": "https://raw.githubusercontent.com/malajvan/linkedmusic-datalake/add_original_content/simssadb/jsonld/context.jsonld",
         "database": "simssadb:",
         "@type": "wd:Q2188189",
-        "@id": "mw:26",
-        "composer": "wd:Q7442579",
-        "genre_style": "wd:Q4692",
+        "@id": "mw:78",
+        "composer": {
+            "@id": "wd:Q143100",
+            "name": "des Prez Josquin"
+        },
+        "genre_style": {
+            "@id": "wd:Q4692",
+            "name": "Renaissance"
+        },
         "files": [
             {
                 "@type": "simssadb_file",
-                "@id": "https://db.simssa.ca/files/101",
-                "file_format": "wd:Q2115",
-                "Last_Pitch_Class": "[9.0]"
+                "@id": "https://db.simssa.ca/files/309",
+                "file_format": {
+                    "@id": "wd:Q2115",
+                    "name": "xml"
+                },
+                "Last_Pitch_Class": "[0.0]"
             },
             {
                 "@type": "simssadb_file",
-                "@id": "https://db.simssa.ca/files/102",
-                "file_format": "wd:Q10610388",
-                "Last_Pitch_Class": "[9.0]"
+                "@id": "https://db.simssa.ca/files/310",
+                "file_format": {
+                    "@id": "wd:Q10610388",
+                    "name": "midi"
+                },
+                "Last_Pitch_Class": "[0.0]"
             },
             {
                 "@type": "simssadb_file",
-                "@id": "https://db.simssa.ca/files/103",
-                "file_format": "wd:Q42332",
+                "@id": "https://db.simssa.ca/files/311",
+                "file_format": {
+                    "@id": "wd:Q42332",
+                    "name": "pdf"
+                },
                 "Last_Pitch_Class": None
             },
             {
                 "@type": "simssadb_file",
-                "@id": "https://db.simssa.ca/files/104",
-                "file_format": "wd:Q27967347",
+                "@id": "https://db.simssa.ca/files/312",
+                "file_format": {
+                    "@id": "wd:Q27967347",
+                    "name": "sibelius"
+                },
                 "Last_Pitch_Class": None
             }
         ]
-    }]
+    }
+]
 with open('./expanded.json','w') as injson:
     injson.write(json.dumps(jsonld.expand(tested), indent=2))
